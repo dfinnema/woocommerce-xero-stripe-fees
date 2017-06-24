7c3b175a4f1cfe25dbdda79e4ab3b670ee0a8b1f
dfinnema <dfinnema@gmail.com>
2017-06-24 22:27:24 +1200

    VersionTest

--- dfc-xero.php
+++ dfc-xero.php
@@ -6,7 +6,7 @@ Plugin URI: https://github.com/dfinnema/WoocommerceXeroStripe
 Description: Calculates and adds the Stripe Fees to your Xero Invoice
 Author: IT Chef
 Author URI: https://itchef.nz
-Version: 0.1
+Version: 0.2
 
 */
 
